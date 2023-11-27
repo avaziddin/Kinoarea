8541328b2006d3f513beffa887707405a8fbeafd
Daler <dsarifkulo021@gmail.com>
2023-11-27 16:27:48 +0500

    fixed

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import axios from "axios"
+const ul = document.querySelector('ul')
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+axios.get('https://api.themoviedb.org/3/movie/now_playing?language=ru', {
+		headers: {
+			Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`
+		}
+	})
+	.then(res => reload(res.data.results, ul))
 
-setupCounter(document.querySelector('#counter'))
+
+function reload(arr, place) {
+	place.innerHTML = ""
+
+	for (let item of arr) {
+		let li = document.createElement('li')
+		let img = document.createElement('img')
+		let span = document.createElement('span')
+
+
+		span.innerHTML = item.title
+		img.src = "https://image.tmdb.org/t/p/original" + item.poster_path
+
+		li.append(img, span)
+		place.append(li)
+	}
+}
