df08fba7bc073ad3dc7261c0652a65def17441bc
Daler <dsarifkulo021@gmail.com>
2023-12-01 15:58:50 +0500

    fixed

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ import { reload } from "./modules/ui";
 const ul = document.querySelector('ul')
 let genre = document.querySelectorAll("p");
 let box = document.querySelector(".genres_box_inner")
+let trailer_min = document.querySelector('.trailer_min')
+let iframe = document.querySelector('iframe')
+const img_ip = import.meta.env.VITE_IMAGE_URL
+const video_url = import.meta.env.VITE_VIDEO_URL
 
 renderHeader()
 
@@ -34,13 +38,36 @@ function reload_genres(arr, place) {
 
 }
 
-getData('/movie/upcoming')
-	.then(res => console.log(res))
-// genre.forEach((element) => {
-// 	element.onclick = () => {
-// 		genre.forEach((element) => {
-// 			element.classList.remove("active_genre");
-// 		});
-// 		element.classList.add("active_genre");
-// 	};
-// });
+function showTrailer(data) {
+	console.log(video_url);
+	iframe.src = video_url + data.key
+}
+
+getData('/discover/movie')
+	.then(res => {
+		for(let item of res.data.results) {
+			let trailer_block = document.createElement('div')
+			let img = document.createElement('img')
+			let p = document.createElement('p')
+
+			img.src = img_ip + item.backdrop_path
+			p.innerHTML = item.title
+
+			trailer_block.classList.add('tr_m')
+
+			trailer_block.append(img, p)
+			trailer_min.append(trailer_block)
+
+			trailer_block.onclick = () => {
+				getData(`/movie/${item.id}/videos`)
+					.then(res => {
+						let video = res.data.results.find(el => el.type === 'Trailer')
+						if(video) {
+							showTrailer(video)
+						} else {
+							trailer_block.classList.add('no-video')
+						}
+					})
+			}
+		}
+	})
